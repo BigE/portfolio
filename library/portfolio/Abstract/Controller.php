3f503c87f614e9654ea9ea16248c6c533be15601
Eric Gach <eric@php-oop.net>
2009-06-20 17:57:49 +0000

    Changing things around to use controllers

--- library/portfolio/Abstract/Controller.php
+++ library/portfolio/Abstract/Controller.php
@@ -11,6 +11,8 @@
  */
 abstract class portfolio_Abstract_Controller
 {
+	protected $_action;
+	
 	/**
 	 *
 	 * @var array
@@ -23,9 +25,42 @@ abstract class portfolio_Abstract_Controller
 	 */
 	protected $_uri;
 
+	/**
+	 *
+	 * @var portfolio_Template
+	 */
+	protected $_view;
+
 	public function __construct()
 	{
+		$this->_view = new portfolio_Template();
+		
+		if (property_exists($this, '_layout')) {
+			$this->_view->setLayout($this->_layout);
+		}
+
 		$this->_uri = new SiTech_Uri();
-		$this->_path = explode('/', ltrim($uri->getPath(), '/'));
+		$parts = explode('/', ltrim($this->_uri->getPath(), '/'));
+		/* get rid of the controller name */
+		array_shift($parts);
+
+		/* now get the action */
+		if (empty($parts[0])) {
+			$this->_action = 'index';
+		} else {
+			$this->_action = $parts[0];
+			array_shift($parts);
+		}
+
+		$this->_path = $parts;
+		$this->_parsePath();
+
+		if (method_exists($this, $this->_action)) {
+			call_user_func(array($this, $this->_action));
+		} else {
+			throw new SiTech_Exception('Invalid action "%s"', array($this->_action));
+		}
 	}
+
+	abstract protected function _parsePath();
 }
