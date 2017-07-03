e16520fb4dba28d63874a03ccfa9b87f9e141610
Eric Gach <eric.gach@gmail.com>
2017-07-03 18:23:29 -0500

    Adding registration and login

---
+++ src/PortfolioBundle/Form/UserType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PortfolioBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+abstract class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'maxlength' => 255,
+                    'placeholder' => 'user@example.net',
+                    'size' => 40,
+                ],
+            ])
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'maxlength' => 100,
+                    'placeholder' => 'h@x0r',
+                    'size' => 30,
+                ]
+            ])
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'maxlength' => 255,
+                    'placeholder' => 'John Doe',
+                    'size' => 30,
+                ],
+                'required' => false,
+            ]);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PortfolioBundle\Entity\User'
+        ));
+    }
+
+}
