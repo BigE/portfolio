11a0cc307999d721c4f5979937074a73e268b328
Eric Gach <eric.gach@gmail.com>
2017-08-14 01:26:44 -0500

    Updating contact form

--- src/PortfolioBundle/Controller/DefaultController.php
+++ src/PortfolioBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace PortfolioBundle\Controller;
 use PortfolioBundle\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -17,26 +18,57 @@ class DefaultController extends Controller
      */
     public function contactAction(Request $request)
     {
+        $json = ['success' => false];
         $contactType = $this->createForm(ContactType::class);
         $contactType->handleRequest($request);
 
-        if ($contactType->isSubmitted() && $contactType->isValid()) {
-            $contact = $contactType->getData();
-            $mailer = $this->get('swiftmailer.mailer.default');
-            $message = (new \Swift_Message('Portfolio Contact'))
-                ->setFrom($contact['email'], $contact['name'])
-                ->setTo('eric.gach@gmail.com', 'Eric Gach')
-                ->addCc($contact['email'], $contact['name'])
-                ->setBody(
-                    $this->renderView('PortfolioBundle:Email:contact.html.twig', [
-                        'contact' => $contact,
-                    ]),
-                    'text/html'
-                )
-            ;
+        if ($contactType->isSubmitted()) {
+            if ($contactType->isValid()) {
+                $contact = $contactType->getData();
+                $mailer = $this->get('swiftmailer.mailer.default');
+                $message = (new \Swift_Message('Portfolio Contact'))
+                    ->setFrom('noreply@ericgach.me', 'No Reply')
+                    ->setTo($contact['email'], $contact['name'])
+                    ->addCC('eric.gach@gmail.com', 'Eric Gach')
+                    ->setBody(
+                        $this->renderView('PortfolioBundle:Email:contact.html.twig', [
+                            'contact' => $contact,
+                        ]),
+                        'text/html'
+                    );
 
-            $mailer->send($message);
-        } else if ($contactType->isSubmitted() && !$contactType->isValid()) {
+                $mailer->send($message);
+                $json['success'] = true;
+                $json['message'] = 'Thank you for contacting me! I will respond to you soon.';
+            } else if ($contactType->isSubmitted() && !$contactType->isValid()) {
+                $json['message'] = [];
+                foreach ($contactType->getErrors() as $error) {
+                    $json['message'][] = $error->getMessage();
+                }
+
+                $json['errors'] = [];
+                foreach ($contactType as $item) {
+                    if (($errors = $item->getErrors())->count() > 0) {
+                        $json['errors'][$item->getName()] = [];
+                        do {
+                            $json['errors'][$item->getName()][] = $errors->current()->getMessage();
+                        } while ($errors->next());
+                    }
+                }
+            }
+
+            if ($request->isXmlHttpRequest()) {
+                return new JsonResponse($json);
+            }
+
+            if (isset($json['errors'])) {
+                $this->addFlash($contactType->getName(), 'Unable to complete contact form, please try again');
+                foreach ($json['errors'] as $key => $error) {
+                    $this->addFlash($contactType->getName() . '_' . $key, implode(',', $error));
+                }
+            } else {
+                $this->addFlash($contactType->getName() . '_success', $json['message']);
+            }
         }
 
         return $this->redirectToRoute('homepage', ['_fragment' => 'contact']);
