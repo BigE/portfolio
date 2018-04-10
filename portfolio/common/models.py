e8586bb27c6d6039f6f94c2aec03d3d7ebdf7a0f
Eric Gach <eric.gach@gmail.com>
2018-04-10 01:41:48 -0500

    Updating navigation menus to be dynamic

--- portfolio/common/models.py
+++ portfolio/common/models.py
@@ -1,12 +1,110 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
 
-from wagtail.admin.edit_handlers import MultiFieldPanel, StreamFieldPanel, FieldPanel, PageChooserPanel
+from wagtail.admin.edit_handlers import MultiFieldPanel, StreamFieldPanel, FieldPanel, PageChooserPanel, InlinePanel
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page, Orderable
+from wagtail.documents.edit_handlers import DocumentChooserPanel
+from wagtail.snippets.models import register_snippet
 
 from .blocks import SectionBlock
 # Create your models here.
+
+
+class LinkFields(models.Model):
+    link_external = models.URLField('external link', blank=True, null=True)
+    link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='+'
+    )
+    link_anchor = models.CharField('page anchor', max_length=100, blank=True, null=True)
+    link_document = models.ForeignKey(
+        'wagtaildocs.Document',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='+'
+    )
+
+    @property
+    def url(self):
+        if self.link_page:
+            url = self.link_page.url
+            if self.link_anchor:
+                url += '#' + self.link_anchor
+            return url
+        elif self.link_document:
+            return self.link_document.url
+        else:
+            return self.link_external
+
+    panels = [
+        FieldPanel('link_external'),
+        MultiFieldPanel([
+            PageChooserPanel('link_page'),
+            FieldPanel('link_anchor'),
+        ], heading='link page'),
+        DocumentChooserPanel('link_document'),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class NavigationMenuItem(Orderable, LinkFields):
+    menu = ParentalKey(to='common.NavigationMenu', related_name='menu_items')
+    menu_icon = models.CharField(max_length=255, blank=True, null=True,
+                                 help_text='Menu icon (font awesome) to show')
+    menu_title = models.CharField(max_length=255, blank=True, null=True,
+                                  help_text='Optional link title in this menu')
+
+    @property
+    def menu_item_title(self):
+        if self.menu_title:
+            return self.menu_title
+        elif self.link_page:
+            return self.link_page.title
+        elif self.link_document:
+            return self.link_document.title
+        else:
+            return self.link_external
+
+    def __str__(self):
+        return self.menu_item_title
+
+    panels = [
+        FieldPanel('menu_title'),
+        FieldPanel('menu_icon'),
+    ] + LinkFields.panels
+
+
+class NavigationMenuManager(models.Manager):
+    def get_by_natural_key(self, menu_location):
+        return self.get(menu_location=menu_location)
+
+
+@register_snippet
+class NavigationMenu(ClusterableModel):
+    objects = NavigationMenuManager()
+    menu_location = models.CharField(null=False, max_length=255, help_text='Template name', unique=True)
+    menu_name = models.CharField(null=True, blank=True, max_length=255)
+
+    def __str__(self):
+        name = self.menu_name
+        if not name:
+            name = 'Unnamed'
+        return '{name} ({location})'.format(name=name, location=self.menu_location)
+
+    panels = [
+        FieldPanel('menu_name'),
+        InlinePanel('menu_items', label='linked pages'),
+        FieldPanel('menu_location'),
+    ]
 
 
 class StandardPage(Page):
@@ -16,47 +114,6 @@ class StandardPage(Page):
 
     class Meta:
         abstract = True
-
-
-class NavigationItem(Orderable, models.Model):
-    icon = models.CharField(max_length=50)
-    text = models.CharField(max_length=250)
-    page = models.ForeignKey(
-        'wagtailcore.Page',
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True,
-        related_name='+'
-    )
-    page_anchor = models.CharField(max_length=50, blank=True, null=True)
-    external_url = models.URLField(blank=True, null=True)
-
-    panels = [
-        FieldPanel('icon'),
-        FieldPanel('text'),
-        MultiFieldPanel([
-            PageChooserPanel('page'),
-            FieldPanel('page_anchor'),
-        ], heading='page'),
-        FieldPanel('external_url'),
-    ]
-
-    @property
-    def url(self):
-        if self.page:
-            url = self.page.url
-            if self.page_anchor:
-                url += '#' + self.page_anchor.__str__()
-            return url
-        else:
-            return self.external_url
-
-    def __str__(self):
-        return self.text
-
-    def clean(self):
-        if self.page is None and self.external_url is None:
-            raise ValidationError('You must specify a page or an external url')
 
 
 class IntroPage(StandardPage):
--- portfolio/common/templatetags/navigation.py
+++ portfolio/common/templatetags/navigation.py
@@ -1,12 +1,14 @@
 from django import template
-from portfolio.common.models import NavigationItem
+from django.template.base import logger
+
+from portfolio.common.models import NavigationMenu
 
 register = template.Library()
 
 
-@register.inclusion_tag('common/tags/navigation.html', takes_context=True)
-def navigation(context):
-    return {
-        'items': NavigationItem.objects.all(),
-        'request': context['request']
-    }
+@register.simple_tag
+def navigation(location):
+    try:
+        return NavigationMenu.objects.get_by_natural_key(location)
+    except NavigationMenu.DoesNotExist:
+        logger.critical('No such navigation menu %s', location)
